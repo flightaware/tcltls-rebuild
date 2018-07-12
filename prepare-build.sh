6fb532f253a4cae2d4c4589b8d304a3d2505a18c
Oliver Jowett <oliver@mutability.co.uk>
2018-07-12 11:33:50 +0100

    Fix up prepare-build.sh:
     * set +x on the script
     * prepare a package in package-$DIST like the other packages do
     * force the right backport version number to reflect the existing jessie backport
     * do a backport build for stretch since the original is from buster, not stretch

--- prepare-build.sh
+++ prepare-build.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 
-# TODO: Handle tcltls versioning number and cd into correct directory
-
 DIST=$1
+
+cp -a tcltls-1.7.16 package-$DIST
+cd package-$DIST
+
+# nb: we force the full version number here to get the correct build number (+2) to reflect the earlier, released,
+# backport we did for FF 7.8.10
+
+BASE=$(dpkg-parsechangelog -S Version)
+BUILD=2
 
 case "$DIST" in
     wheezy)
-        dch --local ~bpo7+ --distribution wheezy-backports --force-distribution "Backport to wheezy"
+        dch --newversion ${BASE}~bpo7+${BUILD} --force-bad-version --distribution wheezy-backports --force-distribution "Backport to wheezy"
         ;;
     jessie)
-        dch --local ~bpo8+ --distribution jessie-backports --force-distribution "Backport to jessie"
+        dch --newversion ${BASE}~bpo8+${BUILD} --force-bad-version --distribution jessie-backports --force-distribution "Backport to jessie"
         ;;
     stretch)
+        dch --newversion ${BASE}~bpo9+${BUILD} --force-bad-version --distribution stretch-backports --force-distribution "Backport to stretch"
         ;;
     *)
         echo "Don't know how to build tcltls for a distribution named $DIST" >&2
